82d66aa8e0dbb7868abf89b387806f0cb551a168
Brayan Martinez <bamartinez@econtainers.co>
2022-07-02 23:42:26 -0500

    Inicializando Proyecto Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'front_angular';
+  title = 'BAM_Chat';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// * Import components
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+// * Import environment
+import { environment } from '@environment/environment';
+// * Import custom modules
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
